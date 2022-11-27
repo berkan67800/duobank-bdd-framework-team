3f07ab1a92a3e9eac24c61be47682dd0f4c060b5
Berkan <bsaridilek@gmail.com>
2022-11-26 20:09:55 -0800

    MortgageApplicationPage locators added

--- src/test/java/com/duobank/pages/MortgageApplicationPage.java
+++ src/test/java/com/duobank/pages/MortgageApplicationPage.java
@@ -1,6 +1,8 @@
 package com.duobank.pages;
 
 import com.duobank.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class MortgageApplicationPage {
@@ -8,5 +10,19 @@ public class MortgageApplicationPage {
     public MortgageApplicationPage(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
+
+    @FindBy(xpath = "//div[@class='loanamount']")
+    public WebElement loanAmountCalculator;
+
+    @FindBy(id = "estimatedprice")
+    public WebElement estimatedPurchasePriceField;
+
+    @FindBy(id = "downpayment")
+    public WebElement downPaymentAmount;
+
+    @FindBy(name ="down_payment_percent")
+    public WebElement downPaymentPercentage;
+
 
 }
