57e15c280c844f78c0f7b5ac6cbb48bd9308a9a6
Berkan <bsaridilek@gmail.com>
2022-12-06 04:30:05 -0800

    parallel test added.

--- src/test/java/com/duobank/stepDefinitions/Hooks.java
+++ src/test/java/com/duobank/stepDefinitions/Hooks.java
@@ -1,6 +1,7 @@
 package com.duobank.stepDefinitions;
 
 import com.duobank.utilities.ConfigReader;
+import com.duobank.utilities.DBUtils;
 import com.duobank.utilities.Driver;
 import io.cucumber.java.*;
 import org.openqa.selenium.OutputType;
@@ -11,8 +12,7 @@ import java.time.Duration;
 public class Hooks {
 
 
-
-    @Before
+    @Before ("not @db_only")
     public void setupScenario(){
 
         Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
@@ -22,15 +22,30 @@ public class Hooks {
 
     }
 
-@After
+    @BeforeAll
+    public static void setupDb(){
+        DBUtils.createConnection();
+    }
+    //
+    @AfterAll
+    public static void tearDb(){
+        DBUtils.close();
+    }
+
+
+    @After ("not @db_only")
     public void tearDownScenario(Scenario scenario){
 
         if(scenario.isFailed()) {
             byte[] screenshotFile = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
             scenario.attach(screenshotFile, "image/png", "screenshotOfFailure");
+
         }
+
         Driver.quitDriver();
+
     }
+
 
 
 }
